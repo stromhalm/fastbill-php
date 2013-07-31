cf4c3722262b1e563937a33d4a4a0472ad85d577
Digitalschmiede <info@pxemup.de>
2013-07-31 10:50:17 +0200

    Create fastbill.1.0.php
    Version 1

---
+++ fastbill.1.0.php
@@ -0,0 +1,56 @@
+<?php
+
+class fastbill
+{
+  private $email = '';
+	private $apiKey = '';
+	private $apiUrl = 'https://portal.fastbill.com/api/1.0/api.php';
+	
+	public function __construct($email, $apiKey)
+	{
+		if($email != '' && $apiKey != '')
+		{
+			$this->email = $email;
+			$this->apiKey = $apiKey;
+			return true;
+		}
+		else
+		{
+			return false;	
+		}
+	}
+	
+	public function request($data)
+	{
+		if($data)
+		{
+			if($this->email != '' && $this->apiKey != '' && $this->apiUrl != '')
+			{
+				$data_string = json_encode($data);
+				
+				$JSON = file_get_contents($this->apiUrl, null, stream_context_create(array(
+				'http' => array(
+				'method' => 'POST',
+				'header' => 'Content-Type: application/json; charset=utf-8' . "\r\n"
+				. 'Content-Length: ' . strlen($data_string) . "\r\n"
+				. 'Authorization: Basic ' . base64_encode($this->email.':'.$this->apiKey),
+				'content' => $data_string,
+				),
+				)));
+				
+				$array = json_decode($JSON);
+				return $array;
+			}
+			else
+			{
+				return false;	
+			}
+		}
+		else
+		{
+			return false;	
+		}
+	}
+}
+
+?>
